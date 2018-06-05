5286969514eee24e4f6d8567c45440437cf156ed
Pierrick <pierrickblons@gmail.com>
2018-06-05 13:11:16 +0200

    step1: Throws argumentException if value is 0

---
+++ FizzBuzz/SQLI.CodingDojo.Tests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SQLI.CodingDojo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SQLI.CodingDojo.Tests
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Throws_ArgumentException_If_Param_is_0()
+        {
+            // Arrange - Autant de lignes de code que l'on veut
+            // Act - 1 seule ligne
+            FizzBuzz.CheckValue(0);
+            // Assert - 1 ou 0 (cas particulier avec MSTEST)
+        }
+    }
+}
